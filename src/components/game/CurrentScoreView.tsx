f6bf2e843f52de3be8ecf5fc1d53a042b93c7d11
jozhe <joze.kosmerl@gmail.com>
2020-05-02 07:27:19 +0800

    Overlay svg image for a counter

--- src/components/game/CurrentScoreView.tsx
+++ src/components/game/CurrentScoreView.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import HeartIcon from '../Icon/HeartIcon';
 
 interface Props {
   currentScore: Number;
 }
 
+const useStyles = makeStyles({
+  container: {
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+  },
+  heartIcon: {
+    display: 'block',
+    width: '100%',
+    height: 'auto',
+  },
+  overlay: {
+    position: 'absolute',
+    top: '30%',
+    width: '100%',
+    zIndex: 1000,
+  },
+});
+
 const CurrentScoreView = (props: Props) => {
-  return <div className='score'><p>Current Entities: {props.currentScore}</p></div>;
+  const styles = useStyles();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.heartIcon}><HeartIcon /></div>
+      <div className={styles.overlay}><div>{props.currentScore}</div></div>
+  </div>);
 };
 
 export default CurrentScoreView;
